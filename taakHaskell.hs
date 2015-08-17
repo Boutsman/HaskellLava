b1c8fb53bfc58d05fdd07b2e46fd48cf6dd17c8c
Boutsman@gmail.com <boutsman@gmail.com>
2015-08-17 11:34:22 +0200

    min, max en teGroot toegevoegd aan taakHaskell

--- taakHaskell.hs
+++ taakHaskell.hs
@@ -4,14 +4,30 @@ insert x (y:ys)
    | x < y     = x:y:ys
    | otherwise = y:(insert x ys)
    
-iSort [] = []
-iSort (x:xs) = insert x (iSort xs)
+sort [] = []
+sort (x:xs) = insert x (sort xs)
 
 som [] = 0
 som (x:xs) = x + som xs
+
+grootsteWaarde x [] = x
+grootsteWaarde x (y:ys)
+	| x < y	= grootsteWaarde y ys
+	| otherwise		= grootsteWaarde x ys
+hoogsteWaarde (x:xs) = grootsteWaarde x xs
+
+kleinsteWaarde x [] = x
+kleinsteWaarde x (y:ys)
+	| x > y	= grootsteWaarde y ys
+	| otherwise		= grootsteWaarde x ys
+laagsteWaarde (x:xs) = grootsteWaarde x xs
 
 -- x = filesize
 -- y = available space
 teGroot x y
 	| som x < som y		= False
 	| otherwise			= True
+	
+-- Extra interessante Functies
+maxInt = maxBound :: Int
+minInt = minBound :: Int
